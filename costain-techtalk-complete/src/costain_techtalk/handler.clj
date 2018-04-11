d61f0b7035783f3569233f8abafd6c4219aa57f3
trickyBytes <rtiffin@gmail.com>
2018-04-11 08:22:18 +0100

    More typing

--- costain-techtalk-complete/src/costain_techtalk/handler.clj
+++ costain-techtalk-complete/src/costain_techtalk/handler.clj
@@ -9,21 +9,25 @@
   (GET "/" [] "Hello Costain")
   (GET "/json" []
        {:status 200
-        :headers {"Content-Type" "text/html"}
+        ;;:headers {"Content-Type" "text/html"}
         :body {:location "Yatton"
-               :unit "12A"}})
-  (GET "/tech-talk" []
+               :unit "12A"
+               :speaker "Rich"}})
+  (GET "/tech-talk/" []
        (h/html5
         [:head [:title "Tech Talk"]]
-        [:body
-         [:h1 "Welcome to techtalks"]
-         [:p
-          [:ul
-           [:li [:a {:href "/"} "Home"]]
-           [:li [:a {:href "/json-example"} "Json"]]]]]))
+        [:body "Tech Talks"]))
+  (POST "/tech-talk/:title"
+        {{title :title
+          length :length} :params :as params}
+       (h/html5
+        [:head [:title title]]
+        [:body [:div
+                [:h1 title]
+                [:p length]]]))
   (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
-      (wrap-defaults site-defaults)
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
       wrap-json-response))
